e24feac980f58a9cf2d4a5b1b2eb384c06da04bf
Hakeemkun <serhanabdelhakim6@gmail.com>
2023-09-14 19:19:31 +0200

    up to date

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,11 @@ export const App = () => {
 
 
 
-  const [navStyle,setNavStyle]=useState(false)
-  const [logoColor,setLogoColor]=useState(false)
-
-  const handleScroll=()=>{
-    if (window.scrollY>20){
-      setLogoColor(true)
-      setNavStyle(true)
-    }
-    else{
-      setLogoColor(false)
-      setNavStyle(false)
-    }
-  }
-
-  useEffect(()=>{
-    window.addEventListener('scroll',handleScroll)
-    return()=>{
-      window.removeEventListener('scroll',handleScroll)
-    }
-  },[]
-  )
-
-
   return (
     <div className='app' >
       <Router>
         <Routes>
-          <Route path='/tesla' element={<Home navStyle={navStyle} logoColor={logoColor} ></Home>} ></Route>
+          <Route path='/tesla' element={<Home ></Home>} ></Route>
           {carsInfo.map((car)=>(
             <Route path={`/${car.model}`} element={<CarInfo img1={car.img1} img2={car.img2} img3={car.img3} title={car.title} price={car.price} speed={car.speed} range={car.range} ></CarInfo>}></Route>
           ))}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,80 +7,104 @@ import { motion } from 'framer-motion';
 
 export const Home = (props) => {
 
+  const [navStyle,setNavStyle]=useState(false)
+  const [logoColor,setLogoColor]=useState(false)
+
+  const handleScroll=(event)=>{
+    if (event.target.scrollTop>20){
+      setLogoColor(true)
+      setNavStyle(true)
+    }
+    else{
+      setLogoColor(false)
+      setNavStyle(false)
+    }
+  }
+
+  const scrolledDiv=useRef()
+
+  useEffect(()=>{
+
+    scrolledDiv.current.addEventListener('scroll',handleScroll)
+    return()=>{
+      scrolledDiv.current.removeEventListener('scroll',handleScroll)
+    }
+  },[]
+  )
 
   return (
     < div className='home' >
-      <Navbar navStyle={props.navStyle} logoColor={props.logoColor} ></Navbar>
+      <Navbar navStyle={navStyle} logoColor={logoColor} ></Navbar>
       <div className='blacklayer' ></div>
-      <div className='homeContent' >
-      <div className='hero'>
-        <motion.div  className='heroContent' initial={{opacity:0}} whileInView={{opacity:1}} viewport={{once:false,margin:'450px'}} transition={{duration:.7}} >
-          <div className='upper'>
-            <h1>Model 3</h1>
-            <h3>from $29,740*</h3>
-          </div>
-          <div className='down'>
-            <Link to={'/model3'} className='order'>
-              Order Now
-            </Link>
-            <Link to={'/'} className='demo'>
-              Demo Drive
-            </Link>
-          </div>
-        </motion.div>
-        <video className='backvideo' src={video1} autoPlay loop muted />
-      </div>
+      <div className='homeContent'onScroll={()=>{handleScroll}} ref={scrolledDiv} >
+        <div className='hero'>
+          <motion.div  className='heroContent' initial={{opacity:0}} whileInView={{opacity:1}} viewport={{once:false,margin:'450px'}} transition={{duration:.7}} >
+            <div className='upper'>
+              <h1>Model 3</h1>
+              <h3>from $29,740*</h3>
+            </div>
+            <div className='down'>
+              <Link to={'/model3'} className='order'>
+                Order Now
+              </Link>
+              <Link to={'/'} className='demo'>
+                Demo Drive
+              </Link>
+            </div>
+          </motion.div>
+          <video className='backvideo' src={video1} autoPlay loop muted />
+        </div>
 
-      <div className='hero2'>
-        <motion.div className='heroContent'initial={{opacity:0}} whileInView={{opacity:1}} viewport={{once:false,amount:'all'}} transition={{duration:.7}} >
-          <div className='upper'>
-            <h1>Model Y</h1>
-            <h3>from $36,090*</h3>
-          </div>
-          <div className='down'>
-            <Link to={'/modely'} className='order'>
-              Order Now
-            </Link>
-            <Link to={'/'} className='demo'>
-              Demo Drive
-            </Link>
-          </div>
-        </motion.div>
-      </div>
+        <div className='hero2'>
+          <motion.div className='heroContent'initial={{opacity:0}} whileInView={{opacity:1}} viewport={{once:false,amount:'all'}} transition={{duration:.7}} >
+            <div className='upper'>
+              <h1>Model Y</h1>
+              <h3>from $36,090*</h3>
+            </div>
+            <div className='down'>
+              <Link to={'/modely'} className='order'>
+                Order Now
+              </Link>
+              <Link to={'/'} className='demo'>
+                Demo Drive
+              </Link>
+            </div>
+          </motion.div>
+        </div>
 
-      <div className='hero3'>
-        <motion.div className='heroContent' initial={{opacity:0}} whileInView={{opacity:1}} viewport={{once:false,amount:'all'}} transition={{duration:.7}} >
-          <div className='upper'>
-            <h1>Model S</h1>
-            <h3>from $72,090*</h3>
-          </div>
-          <div className='down'>
-            <Link to={'/models'} className='order'>
-              Order Now
-            </Link>
-            <Link to={'/'} className='demo'>
-              Demo Drive
-            </Link>
-          </div>
-        </motion.div>
-      </div>
+        <div className='hero3'>
+          <motion.div className='heroContent' initial={{opacity:0}} whileInView={{opacity:1}} viewport={{once:false,amount:'all'}} transition={{duration:.7}} >
+            <div className='upper'>
+              <h1>Model S</h1>
+              <h3>from $72,090*</h3>
+            </div>
+            <div className='down'>
+              <Link to={'/models'} className='order'>
+                Order Now
+              </Link>
+              <Link to={'/'} className='demo'>
+                Demo Drive
+              </Link>
+            </div>
+          </motion.div>
+        </div>
 
-      <div className='hero4'>
-        <motion.div className='heroContent' initial={{opacity:0}} whileInView={{opacity:1}} viewport={{once:false,amount:'all'}} transition={{duration:.7}} >
-          <div className='upper'>
-            <h1>Model X</h1>
-            <h3>from $68,090*</h3>
-          </div>
-          <div className='down'>
-            <Link to={'/modelx'} className='order'>
-              Order Now
-            </Link>
-            <Link to={'/'} className='demo'>
-              Demo Drive
-            </Link>
-          </div>
-        </motion.div>
-      </div>
+        <div className='hero4'>
+          <motion.div className='heroContent' initial={{opacity:0}} whileInView={{opacity:1}} viewport={{once:false,amount:'all'}} transition={{duration:.7}} >
+            <div className='upper'>
+              <h1>Model X</h1>
+              <h3>from $68,090*</h3>
+            </div>
+            <div className='down'>
+              <Link to={'/modelx'} className='order'>
+                Order Now
+              </Link>
+              <Link to={'/'} className='demo'>
+                Demo Drive
+              </Link>
+            </div>
+          </motion.div>
+        </div>
       </div>
     </div>
   );
